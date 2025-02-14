4db279037e842d3ce89d16431809145a99f481fb
Phan Thi Linh Tra <phanthilinhtra25@gmail.com>
2025-02-14 11:58:49 +0100

    2. feladat kész

--- fajlbeolvasas/Program.cs
+++ fajlbeolvasas/Program.cs
@@ -2,17 +2,6 @@
 {
 	internal class Program
 	{
-		static void Main(string[] args)
-		{
-			List<Karakter> karakterek = [];
-			
-			Beolvasas("karakterek.txt", karakterek);
-
-			foreach (var item in karakterek)
-			{
-                Console.WriteLine(item); //Console.WriteLine(item.ToString);
-			}
-		}
 
 		static void Beolvasas(string filenev, List<Karakter> karakterek)
 		{
@@ -30,6 +19,46 @@
 				Karakter karakter = new Karakter(szavak[0], Convert.ToInt16(szavak[1]), Convert.ToInt16(szavak[2]), Convert.ToInt16(szavak[3]));
 				karakterek.Add(karakter);
 			}
+		}
+
+		class BootlegList<T>
+		{
+			private T[] elements;
+			public T
+				this[int index]
+			{ get { return elements[index]; } set { elements[index] = value; } }
+		}
+
+		static void LegMagasabbEletero(List<Karakter> karakterek)
+		{
+			Karakter mhp = karakterek[0];
+			foreach (var character in karakterek.Skip(1))
+			{
+				if (mhp.Eletero < character.Eletero)
+				{
+					mhp = character;
+				}
+			}
+            Console.WriteLine($"\nLegmagasabb életerő: {mhp.Eletero}");
+            Console.WriteLine(mhp);
+			//Karakter vmi = karakterek[2];
+		    //Console.WriteLine(vmi);
+			//var names = karakterek.Select(it => it.Szint);
+			//int maxHP = karakterek.Select(it => it.Eletero).Max();
+			//Console.WriteLine(maxHP);
         }
+		static void Main(string[] args)
+		{
+			List<Karakter> karakterek = [];
+			
+			Beolvasas("karakterek.txt", karakterek);
+
+			foreach (var item in karakterek)
+			{
+                Console.WriteLine(item); //Console.WriteLine(item.ToString);
+			}
+
+			LegMagasabbEletero(karakterek);
+		}
 	}
 }
